17e745f78b46a8a0da0d0ec721266d100c6ddc41
boput <bozidar@putanec.com>
2024-12-31 10:52:53 -0800

    Some chnages

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+return {
+--   { "rose-pine/neovim", name = "rose-pine" },
+
+--   vim.cmd("colorscheme rose-pine"),
+
+--     -- vim-po for gettext files
+--     'boput/vim-po',
+}
